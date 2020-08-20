37d9dc24cdd22fa9f4066f731b61a8bfec308dbf
mad_max_prog <qweasdqwe941@gmail.com>
2020-08-20 18:24:30 +0300

    leason 3

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -4,7 +4,27 @@
 
 @section('content')
     <h1>New page</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi accusantium facere culpa rerum? Hic, exercitationem nisi. Atque quas sint natus tempora error, fuga similique rem adipisci placeat molestiae eaque iste.</p>
+    <form action="{{ route('contact-form') }}" method='post'>
+        @csrf
+
+        <div class="form-group">
+            <label for="name">Type in name</label>
+            <input type="text" placeholder="Type in your name" name='name' id="name" class=form-control>
+        </div>
+
+        <div class="form-group">
+            <label for="email">Type in email</label>
+            <input type="text" placeholder="Type in your emeil" name='email' id="email" class=form-control>
+        </div>
+
+        <div class="form-group">
+            <label for="message">Type in message</label>
+            <textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="message"></textarea>
+        </div>
+        
+
+        <button class="btn btn-success" type="submit">Send</button>
+    </form>
 @endsection
 
 @section('aside')
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/new',function(){
     return view('new');
-});
+})->name('new-page');
+
+Route::post('/new/submit/test',function(){
+    dd(Request::all());
+})->name('contact-form');
